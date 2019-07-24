a9b03cac4a1033ff8b93968e6182faaa3ec8d00b
tactfulness <pvenkat.rv@gmail.com>
2019-07-24 08:46:10 +0530

    d-tree

--- supervised-learning/decision-tree.py
+++ supervised-learning/decision-tree.py
@@ -11,7 +11,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
 data = pd.read_csv("https://raw.githubusercontent.com/trainervenkat/MDUSIT/master/datasets/iris_data.csv")
@@ -20,21 +19,21 @@ print(data.head())
 features = data[["SepalLength","SepalWidth","PetalLength","PetalWidth"]]
 targets = data.Class 
 
-feature_train, feature_test, target_train, target_test = train_test_split(features, targets, test_size=5)
+feature_train, feature_test, target_train, target_test = train_test_split(features, targets, test_size=2)
 
 '''Gini index approach is faster since we don't have to compute 
 log values whcih are expensive'''
-model = DecisionTreeClassifier(criterion='gini')
+#model = DecisionTreeClassifier(criterion='gini')
 
-# model = DecisionTreeClassifier(criterion='entropy')
+model = DecisionTreeClassifier(criterion='entropy')
 
 model.fitted = model.fit(feature_train, target_train)
 model.predictions = model.fitted.predict(feature_test)
 
-print(confusion_matrix(target_test, model.predictions))
-print(accuracy_score(target_test, model.predictions))
+print('confusion matrix: ',confusion_matrix(target_test, model.predictions))
+print('accuracy score: ',accuracy_score(target_test, model.predictions))
 
 
 #validate preformance with cross validation
 scores=cross_val_score(model,features,targets,cv=5)
-print("cross-validation:",scores.mean())
+print("cross-validation: %3f"%(scores.mean()))
